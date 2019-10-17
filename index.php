de690871b7068edb074c32ad1905e407244a84f1
Todd Murphy <tmurphy605@icloud.com>
2019-10-17 16:12:51 -0400

    added api to homepage

--- index.php
+++ index.php
@@ -186,12 +186,9 @@ if (!$_SESSION['logged']){
       <li><a href='changepassword.html'>Change Password</a></li>
       <li><a href='handler_logout.php'>Logout</a></li>
 
-        <form action='/action_page.php'>
-          <li><input type='text' placeholder='Search..' name='search'></li>
+      <input id = 'movieTitle'  name = 'movieTitle' placeholder='Enter Movie name' autocomplete='off' min='0' required>
+        <button id = 'btn' type = 'BUTTON'><b><font color= ' #008000'>Search Movie</font></b></button>
 
-          <button type='submit'>Submit</button>
-
-        </form>
 
     </ul>
     ";
@@ -235,6 +232,12 @@ Latest Movies!
   <span class="dot" onclick="currentSlide(3)"></span> 
 </div>
 
+<div>
+    Movie Information:
+    <br>
+</div>
+<div id= "B"></div>
+
 <script>
 
 var slideIndex = 0;
@@ -256,6 +259,43 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
+
+</script>
+    
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        
+<script type = "text/javascript"> 
+ $(document).ready( function(){
+
+   $("button").click(function(){ 
+
+     var movieTitle = $("#movieTitle").val();
+
+     if(movieTitle != ''){
+
+         $.ajax({
+             type:         "GET",
+             url:         "API.php",
+             data:         "movieTitle="+movieTitle,
+
+             beforeSend: function(){         
+                $("#B").html("Grabbing Movie Data....");
+             },
+
+             error: function(xhr, status, error) {  
+                alert( "Error Mesaage:  \r\nNumeric code is: "  + xhr.status + " \r\nError is " + error);   
+             },
+
+             success: function(result){
+                r = JSON.parse(result);
+                res = "<br> Movie Name: "+r.Title+"<br><br> Year: "+r.Year+"<br> <br> Rated: "+r.Rated+"<br><br> Genre: "+r.Genre+"<br>";
+
+                $("#B").html(res);
+            }
+        });
+     };    
+  });   
+});                
 
 </script>
 
