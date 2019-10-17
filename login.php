d3be9eb2e93cbce18edf8e4127a8528e194e5c09
Todd Murphy <tmurphy605@icloud.com>
2019-10-17 15:39:26 -0400

    fixed some errors in code

--- login.php
+++ login.php
@@ -148,13 +148,13 @@ if (!$_SESSION['logged']){
 
     <label for="psw"><b>Password</b></label><br>
     <input type="password"  placeholder="Enter Password" name="password" required >
-        
+  </div>
 
 
 
   <div class="container" style="background-color:#f1f1f1">
 	 <button type="submit" class ="loginbtn">Login</button>
-    <button  type="button" style="background-color:#FF0000"  > <a href = "index.html">Cancel</button></a>
+      <button  type="button" style="background-color:#FF0000"> <a href = "index.html">Cancel</a></button>
     <span class="psw">Forgot <a href="#">password?</a></span>
   </div>
 </form>
