5e0798a7b8669b8284da7191359573da018d751b
steelermike21 <54916567+steelermike21@users.noreply.github.com>
2019-10-24 15:45:40 -0400

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -4,7 +4,7 @@
     include("usefulfunctions.php");
 
     //check to see if user is logged in
-    loginCheck();
+    //loginCheck();
 
 ?>
 <html>
@@ -73,12 +73,26 @@ img {vertical-align: middle;}
   <li><a href="handler_logout.php">Logout</a></li>
 
 </ul>
-
+<div style="width:100%;">
+<div style="float:left; width:50%;">
 <h1><b>Welcome back!</b></h1>
 
 <img src="popcorn.jpg" style="width:300px; height:300px;" class = "center" align ="auto">
 
-<h2>Here are your latest movie preferences from your survey!</h2>
+<h2>Here are your latest movie preferences!</h2>
+
+</div>
+
+<div style= "float:right; width:50%">
+ <h1>  Movie Information:</h1>
+  <input id = 'movieTitle' name = 'movieTitle' placeholder='Enter Movie name' autocomplete='off' min='0' required>
+  <button id = 'btn' type = 'BUTTON'><b><font color= ' #008000'>Search Movie</font></b></button>
+  
+   <br>
+
+<div id= "B"></div>
+</div>
+</div>
 
 </body>
 
