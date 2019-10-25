94084e78bce322b8a97f7c9a9191dfc7db906fcc
steelermike21 <54916567+steelermike21@users.noreply.github.com>
2019-10-25 14:54:36 -0400

    updated spelling in JS

--- dashboard.php
+++ dashboard.php
@@ -136,7 +136,7 @@
                                  data:         "movieTitle="+movieTitle,
 
                                  beforeSend: function(){         
-                                    $("#B").html("Removing Movies like this from your reccomended list...");
+                                    $("#B").html("Removing Movies like this from your recommended list...");
                                  },
 
                                  error: function(xhr, status, error) {  
@@ -160,7 +160,7 @@
                                  data:         "movieTitle="+movieTitle,
 
                                  beforeSend: function(){         
-                                    $("#B").html("Adding Movies like this to your reccomended list...");
+                                    $("#B").html("Adding Movies like this to your recommended list...");
                                  },
 
                                  error: function(xhr, status, error) {  
