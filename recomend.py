56fcc059be7ceec85a013e400c81379388c6a1df
het19p <48565311+het19p@users.noreply.github.com>
2019-10-25 14:47:16 -0400

    Update recomend.py
    changed input to argv

--- recomend.py
+++ recomend.py
@@ -1,57 +1,63 @@
 import pandas as pd
 import numpy as np
+import sys
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 def get_title_from_index(index):
-	return df[df.index == index]["title"].values[0]
+return df[df.index == index]["title"].values[0]
 
 def get_index_from_title(title):
-	return df[df.title == title]["index"].values[0]
+return df[df.title == title]["index"].values[0]
 
 
-##Step 1: Read CSV File
+#Open and read CSV File
 df = pd.read_csv("movie_metadata_revised.csv")
 #print df.columns
-##Step 2: Select Features
+#select attributes of each movie that will be used for calculation
 
-features = ['keywords','cast','genres','director']
-##Step 3: Create a column in DF which combines all selected features
-for feature in features:
-	df[feature] = df[feature].fillna('')
+attributes = ['keywords','cast','genres','director']
+#create a column in DF which combines all selected attributes
+for att in attributes:
+df[att] = df[att].fillna('')
 
-def combine_features(row):
-	try:
-		return row['keywords'] +" "+row['cast']+" "+row["genres"]+" "+row["director"]
-	except:
-		print("Error:", row)
+def combine_attributes(row):
+try:
+return row['keywords'] +" "+row['cast']+" "+row["genres"]+" "+row["director"]
+except:
+print("Error:", row)
 
-df["combined_features"] = df.apply(combine_features,axis=1)
+df["combined_attributes"] = df.apply(combine_attributes,axis=1)
 
-#print "Combined Features:", df["combined_features"].head()
+#print "Combined attributes:", df["combined_features"].head()
 
-##Step 4: Create count matrix from this new combined column
+#create count matrix from this new combined column
 cv = CountVectorizer()
 
-count_matrix = cv.fit_transform(df["combined_features"])
+count_matrix = cv.fit_transform(df["combined_attributes"])
 
-##Step 5: Compute the Cosine Similarity based on the count_matrix
-cosine_sim = cosine_similarity(count_matrix) 
-movie_user_likes = input("Enter Movie Name:")
-movie_user_likes = movie_user_likes.title() 
+#compute cosine similarity based on the count_matrix
+cosine_sim = cosine_similarity(count_matrix)
 
-## Step 6: Get index of this movie from its title
+movie_user_likes = sys.argv[1] #input("Enter Movie Name:")
+movie_user_likes = movie_user_likes.title()
+
+#print ("enter movie name", sys.arg["enter movie name"])
+
+
+#get index of this movie from its title
 movie_index = get_index_from_title(movie_user_likes)
 
 similar_movies =  list(enumerate(cosine_sim[movie_index]))
 
-## Step 7: Get a list of similar movies in descending order of similarity score
+#get a list of similar movies(most similar to least similar)
 sorted_similar_movies = sorted(similar_movies,key=lambda x:x[1],reverse=True)
 
-## Step 8: Print titles of first 50 movies
+#recommend 5 most similar movies
 i=0
 for element in sorted_similar_movies:
-		print(get_title_from_index(element[0]))
-		i=i+1
-		if i>10:
-			break
+print(get_title_from_index(element[0]))
+i=i+1
+if i>5:
+break
+
