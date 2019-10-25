4d48db6f592282f1d991afe2d73e78221d17eda4
steelermike21 <54916567+steelermike21@users.noreply.github.com>
2019-10-25 14:05:21 -0400

    created initial page

---
+++ preferences.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+
+ ul {
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+          overflow: hidden;
+          background-color: #FF0000;
+        }
+        li {
+          float: left;
+        }
+        li a {
+          display: block;
+          color: white;
+          text-align: center;
+          padding: 14px 16px;
+          text-decoration: none;
+        }
+        li a:hover {
+          background-color: #111;
+        }
+		
+
+</style>
+</head>
+<body>
+
+<ul>
+ <li><a href="index.html">Home</a></li>
+	  <li><a href="dashboard.php">Dashboard</a></li> 
+	  	  <li><a href="preferences.php">Likes/Dislikes</a></li> 
+  <li><a href="changepassword.php">Change Password</a></li>
+ 
+ <li><a href="handler_logout.php">Logout</a></li>
+
+
+</ul>
+
+
+</body>
+</html>
