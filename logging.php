2decf09531ea8a29c45fe9a421cb75e7fbc709f1
steelermike21 <54916567+steelermike21@users.noreply.github.com>
2019-10-31 12:35:18 -0400

    logging function test

---
+++ logging.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function LogMessage($e,$extFile){
+	
+	$file=__FILE__.PHP_EOL;
+	$user= explode("/",$file);
+	$string= trim(preg_replace('/\s+', ' ', $extFile));
+	
+	$loggingmsg= array();
+	$loggingmsg['date'] = date("M-d-y");
+	$loggingmsg['day'] = date("l");
+	$loggingmsg['time']= date("h:i:sa");
+	$loggingmsg['user']= $user[2];
+	$loggingmsg['text']=$e;
+	$loggingmsg['file']= $string;
+	
+	$message= implode(" - ", $loggingmsg);
+	
+	error_log($message.PHP_EOL,3,"../logs/logfile.log");
+}
+
+
+?>
