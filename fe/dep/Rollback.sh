586f73904dccb2c638c75824ef577cea7908462e
Todd Murphy <tam1998@comcast.net>
2019-12-05 15:52:35 -0500

    fixed upload script

--- fe/dep/Rollback.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Rollback Script"
-printf "Please enter the destination you would like to rollback (QA_DMZ, Prod_FE, etc.):  "
-read destination
-
-ssh deployment@"$destination" "mv /var/www/html/backup/* /var/www/html/; rmdir /var/www/html/backup;"
-
-echo "Rollback finished."
